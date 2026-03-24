6fc543ff858a74433bdc34d1a60f2024077a6f80
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-03-24 21:05:37 +0530

    Create assignment1_identity_matrix.py

---
+++ assignment1_identity_matrix.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def create_identity_matrix():
+    matrix = np.identity(4)
+    print("4x4 Identity Matrix:\n", matrix)
+
+if __name__ == "__main__":
+    create_identity_matrix()
